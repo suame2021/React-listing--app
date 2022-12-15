09058d8265240a2c1eda5be82d05b0a088c0f739
suame2021 <85235483+suame2021@users.noreply.github.com>
2022-12-15 01:55:49 +0100

    hero

--- listing-app/src/componenet/Header/header.js
+++ listing-app/src/componenet/Header/header.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import './header.css';
+import React from "react";
+import "./header.css";
 
 const header = () => {
-    return (
-      <div>
-        <div className="header-section">
-          <div className="hero-text">
-            <span className="hero-text-tittle">Listings </span>
-            <span className="hero-text-subtittle">Applications Listing </span>
-          </div>
-          <img src="https://images.pexels.com/photos/572897/pexels-photo-572897.jpeg?auto=compress&cs=tinysrgb&w=600"></img>
+  return (
+    <div>
+      <div className="header-section">
+        <div className="hero-text">
+          <span className="hero-text-tittle">Listings </span>
+          <span className="hero-text-subtittle"> Listing Applications </span>
         </div>
+        <img
+          className="hero-img"
+          src="https://images.pexels.com/photos/572897/pexels-photo-572897.jpeg?auto=compress&cs=tinysrgb&w=600"
+        ></img>
       </div>
-    );
+    </div>
+  );
 };
 
 export default header;
